ea7b33bfae9f4bc955265747a8b70afc7ee711fc
Ultra-dev123 <azmguitar3@gmail.com>
2024-07-10 21:48:44 -0300

    adding styles and structure

--- js/script.js
+++
@@ -1,7 +0,0 @@
-let display = document.getElementById('display');
-let number = 0;
-
-function add() {
-  number += 1;
-  display.textContent = number + "clicks"; // Use textContent to set the text content of the element
-}
